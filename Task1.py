fd69017515fc5349300c395cffc2e5519458b172
Svetlana Popova <sspopova10-08@yandex.ru>
2022-05-26 19:47:02 +0300

    Task 1 fixed: function used

--- Task1.py
+++ Task1.py
@@ -1,8 +1,11 @@
 # Сформировать список из N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-number = int(input('Введите количество членов последовательности: '))
-list_of_numbers = []
-for i in range(number):
-    list_of_numbers.append((-3)**i)
-print(list_of_numbers)
+def form_list (number):
+    list_of_numbers = []
+    for i in range(number):
+        list_of_numbers.append((-3)**i)
+    print(f'Для N = {user_number}: {list_of_numbers}')
+
+user_number = int(input('Введите количество членов последовательности, N = '))
+form_list (user_number)
