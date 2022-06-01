a25b6c55e5a1ee931bd14738c2f0adc1e0b164d0
Svetlana Popova <sspopova10-08@yandex.ru>
2022-06-02 00:48:41 +0300

    Seminar2/Task3 completed

--- Seminar2/Task3.py
+++ Seminar2/Task3.py
@@ -1,20 +1,21 @@
 # В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов.
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-# def difference_max_min(list):
-#     for i in range(0, len(list)):
-#         if list[i] % 1 != 0:
-#             if i == 0:
-#                 max = list[i]
-#                 min = list[i]
-#             if max < list[i]:
-#                 max = list[i]
-#             if min > list[i]:
-#                 min = list[i]
-#     return max - min
+def difference_max_min(list):
+    for i in range(0, len(list)):
+        if list[i] % 1 != 0:
+            list[i] = float('0.' + (str(list[i]).split('.'))[1])
+            if i == 0:
+                max = list[i]
+                min = list[i]
+            if max < list[i]:
+                max = list[i]
+            if min > list[i]:
+                min = list[i]
+    return max - min
 
 
-# float_list = [1.1, 1.2, 3.1, 5, 10.01]
-# result = difference_max_min(float_list)
-
-# print(f'Разница между MAX и MIN = {result}')
+float_list = [1.1, 1.2, 3.1, 5, 10.01]
+print(f'Исходный список = {float_list}')
+result = difference_max_min(float_list)
+print(f'Разница между MAX и MIN дробной части элементов списка = {result}')
