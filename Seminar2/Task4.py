3eee48ada6597bcd9f243537f496f16f5a3588c5
Svetlana Popova <sspopova10-08@yandex.ru>
2022-06-02 01:18:26 +0300

    Seminar2/Task4 corrected: used '.join()' instead of a cycle 'for'

--- Seminar2/Task4.py
+++ Seminar2/Task4.py
@@ -4,14 +4,25 @@
 def dec_to_bin(number):
     binary = ''
     get_list = []
-    while number != 0:
+    if number == 0:
+        return '0'
+    while number != 0:        
         get_list.insert(0, number % 2) # добавляет указанный элемент в список на указанную позицию
         number = number // 2
     get_list = list(map(str, get_list))
-
-    for i in get_list:
-        binary += i
+    binary = "".join(get_list)    
     return binary
+
+
+# более оптимальный вариант
+# def dec_to_bin(number):
+#     binary = ''
+#     if number == 0:
+#         return '0'
+#     while number > 0:
+#         binary = str(number % 2) + binary
+#         number //= 2
+#     return binary
 
 
 user_number = int(input('Введите целое число: '))
